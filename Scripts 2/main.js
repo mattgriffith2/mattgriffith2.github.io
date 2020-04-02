fe4ee305b81b169be3a99010bf20a155c4ae0785
mattgriffith2 <mattgriffith2@gmail.com>
2020-04-01 21:00:30 -0400

    k
    k

---
+++ Scripts 2/main.js
@@ -0,0 +1,11 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Looping through images */
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+thumbBar.appendChild(newImage);
