a967d08a19ff8890b7bcc6d7e548650671529198
mattgriffith2 <mattgriffith2@gmail.com>
2020-04-12 19:35:09 -0400

    Create sample.js

---
+++ lab_8/sample.js
@@ -0,0 +1,13 @@
+function callbackFnDemo(arg1, cb) {
+    if(arg1 === x){
+        cb = arg1 * 2;
+    }
+} 
+
+callbackFnDemo(2, (x) => {
+    callbackFnDemo(x, function (x){
+        callbackFnDemo(3, () => {
+            x
+        })
+    })
+})
