17ecfd6ccb6564593f3281ff3e884b26486b066e
mattgriffith2 <mattgriffith2@gmail.com>
2020-12-14 22:04:33 -0500

    hi

--- NODE-MESSAGEBIRD-VERIFY/index.js
+++ NODE-MESSAGEBIRD-VERIFY/index.js
@@ -1,31 +1,34 @@
 // Load Dependencies
 var express = require('express');
-var exphbs = require('express-handlebars;')
+var exphbs  = require('express-handlebars');
 var bodyParser = require('body-parser');
 
-//Load and initialize MessageBird SDK
-var messagebird = require('messagebird')('m16LxvyAvZFgTydNYL4kiLF32');
+// Load configuration from .env file
+require('dotenv').config();
 
-// set up and configure the express framework
+// Load and initialize MesageBird SDK
+var messagebird = require('messagebird')(process.env.MESSAGEBIRD_API_KEY);
+
+// Set up and configure the Express framework
 var app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-app.use(bodyParser.urlencoded({ extended : true}));
+app.use(bodyParser.urlencoded({ extended : true }));
 
-//Display page to ask the user for their phone number
+// Display page to ask the user for their phone number
 app.get('/', function(req, res) {
     res.render('step1');
 });
 
-//Handle phone number submission
+// Handle phone number submission
 app.post('/step2', function(req, res) {
     var number = req.body.number;
-
-    //Make request to verify api
+    
+    // Make request to Verify API
     messagebird.verify.create(number, {
         originator : 'Code',
-        template: "Your Verification Code is %token"
-    }, function(err, response) {
+        template : 'Your verification code is %token.'
+    }, function (err, response) {
         if (err) {
             // Request has failed
             console.log(err);
@@ -33,34 +36,36 @@ app.post('/step2', function(req, res) {
                 error : err.errors[0].description
             });
         } else {
-            //Request was successful
+            // Request was successful
             console.log(response);
             res.render('step2', {
                 id : response.id
             });
         }
-    });
+    })    
 });
 
-//Verify whether the token is correct
+// Verify whether the token is correct
 app.post('/step3', function(req, res) {
     var id = req.body.id;
     var token = req.body.token;
 
-    //Make request to verify api
-    messagebird.verify.verify(id,token, function(err,response) {
+    // Make request to Verify API
+    messagebird.verify.verify(id, token, function(err, response) {
         if (err) {
-            //verification has failed
+            // Verification has failed
+            console.log(err);
             res.render('step2', {
                 error: err.errors[0].description,
                 id : id
             });
         } else {
-            // verification was successful
+            // Verification was successful
+            console.log(response);
             res.render('step3');
         }
-    });
+    })    
 });
 
-//Start the application
+// Start the application
 app.listen(8080);
